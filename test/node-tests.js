6b19de4ed7f6c7a2df4dc889eefdfdde2a2ec36b
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2022-05-20 14:17:45 -0700

    A few tests for HTML string generation

--- test/node-tests.js
+++ test/node-tests.js
@@ -4,9 +4,23 @@ var expect = require('chai').expect;
 var index = require('../index');
 
 describe('Web Server', function() {
-    it('exists', function() {
-        var result = index.testFunc(3);
+    it('generates a string for the signup page', function() {
+        var result = index.generateHTMLString('html/htmlsection-signup.html');
+        expect(result).to.be.a('string');
+    });
 
-        expect(result).to.equal(4);
+    it('generates a string for the code confirmation screen', function() {
+        var result = index.generateHTMLString('html/htmlsection-confirm.html');
+        expect(result).to.be.a('string');
+    });
+
+    it('generates a string for the login page', function() {
+        var result = index.generateHTMLString('html/htmlsection-login.html');
+        expect(result).to.be.a('string');
+    });
+
+    it('generates a string for the loading screen', function() {
+        var result = index.generateHTMLString('html/htmlsection-loading.html');
+        expect(result).to.be.a('string');
     });
 });
