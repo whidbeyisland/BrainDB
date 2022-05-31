9fc01a71ac0421a03e5e79dd56ccca4f6ec9a595
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2022-05-31 16:21:43 -0700

    Creating major areas for tests

--- test/node-tests.js
+++ test/node-tests.js
@@ -3,24 +3,42 @@
 var expect = require('chai').expect;
 var index = require('../index');
 
-describe('Web Server', function() {
+describe('Signup', function() {
     it('generates a string for the signup page', function() {
         var result = index.generateHTMLString('html/htmlsection-signup.html');
         expect(result).to.be.a('string');
     });
+});
 
+describe('Confirmation', function() {
     it('generates a string for the code confirmation screen', function() {
         var result = index.generateHTMLString('html/htmlsection-confirm.html');
         expect(result).to.be.a('string');
     });
+});
 
+describe('Login', function() {
     it('generates a string for the login page', function() {
         var result = index.generateHTMLString('html/htmlsection-login.html');
         expect(result).to.be.a('string');
     });
+});
 
+describe('Loading Screen', function() {
     it('generates a string for the loading screen', function() {
         var result = index.generateHTMLString('html/htmlsection-loading.html');
         expect(result).to.be.a('string');
     });
+});
+
+describe('Card Generation', function() {
+
+});
+
+describe('Local Storage, Cards', function() {
+
+});
+
+describe('Remote Storage, Cards', function() {
+
 });
